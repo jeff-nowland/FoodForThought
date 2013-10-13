91f8024034567603c36bf1e4d1108b28cb6ce0db
Jeffrey Nowland <jnowland@comcast.net>
2013-10-12 21:13:58 -0500

    forgot testing of slice, that is important

--- src/f4t/f4t.h
+++ src/f4t/f4t.h
@@ -9,6 +9,12 @@ namespace f4t
 	template <typename T>
 	inline T clamp(T value, T lower, T upper)
 	{ return value < lower ? lower : upper < value ? upper : value; }
+
+	typedef double scalar;
+
+	inline double bounded(scalar value)
+	{ return clamp(value, -1.0e20, +1.0e20); }
+
 }
 
 #endif
